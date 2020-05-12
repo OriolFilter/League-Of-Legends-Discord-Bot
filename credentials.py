a063078f4bbfe32e16f0578b71e9b742659e83a7
root <root@raspberrypi>
2020-05-12 03:27:31 +0200

    updated

---
+++ credentials.py
@@ -0,0 +1,15 @@
+### Riot KEY
+
+key="RIOT API-KEY"
+
+### Discord KEY
+
+TOKEN = "DISCORD-API"
+
+### DATABASE
+
+userDB='USER'
+passDB='PASSWORD'
+dbName='DATABASENAME'
+PORT=3306 #Default might be 3306
+HOST='IP/LOCALHOST/HOST THAT HOLDS THE DATABASE'
